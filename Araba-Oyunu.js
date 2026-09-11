863f6d86f8df34218a26b54f049969bdde8beeb1
Primus55-Cloud <senkoray.ks@gmail.com>
2026-09-11 14:15:08 +0300

    renk geldi

--- Araba-Oyunu.js
+++ Araba-Oyunu.js
@@ -3,4 +3,9 @@ var c = docunent.creatElement("canvas");
 c.width = window.innerWidth;
 c.height = window.innerHeight;
 document.body.appendChild(c);
+var ctx = c.getContext("2d");
 
+
+
+
+ctx.fillReckt(0,0,c.width,c.height);
